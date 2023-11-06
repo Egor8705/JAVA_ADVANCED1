4946a768326670d1181536f55a3edb61f5ccf997
ELano <karadeniz8705@gmail.com>
2023-11-07 02:04:18 +0300

    Exercise-6: Fix OperationException.

--- src/main/java/org/example/calculator/OperationException.java
+++ src/main/java/org/example/calculator/OperationException.java
@@ -3,10 +3,10 @@ package org.example.calculator;
 public class OperationException extends RuntimeException {
 
     private static final String message = "Incorrect operation, please, input operation again (+, -, *, /)";
-    
+
 
     @Override
     public String toString() {
-        return message + "Please, input operation again (+, -, *, /)";
+        return message;
     }
 }
